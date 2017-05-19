745daa87da2750a63b03f709672739fde403f483
duoli <duoli@qangjubao.com>
2017-05-19 16:27:19 +0800

    源文件

--- js/index.js
+++ js/index.js
@@ -3,10 +3,28 @@
  */
 define(['jquery','handlebars'], function ($,handlebars) {
     var data = {};
+    var myTemplate = handlebars.compile($('#table-template').html());
+    // handlebars.registerHelper('addOne',function (index) {
+    //     this._index = index + 1;
+    //     return this._index;
+    // });
+     handlebars.registerHelper('compare',function (v1,v2,options) {
+         if(v1>v2){
+             return options.fn(this);
+         } else {
+             return options.inverse(this);
+         }
+     });
+    handlebars.registerHelper('transformat',function (value) {
+        if(value == 0){
+            return "男";
+        } else if(value == 1) {
+            return "女";
+        }
+    })
     $(document).ready(function () {
-        $.getJSON('./data/each.json',function (resp) {
+        $.getJSON('./data/if.json',function (resp) {
             data = resp;
-            var myTemplate = handlebars.compile($('#table-template').html());
             $('#tableList').html(myTemplate(data));
         });
 
